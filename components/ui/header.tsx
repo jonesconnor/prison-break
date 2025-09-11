293d05be6f77e109e0dc0612fa96ce28ed3a77b0
Harry McKinney <harrymckinney@Harrys-MacBook-Air.local>
2025-09-10 18:35:17 -0700

    Fixing linting issues

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -9,11 +9,11 @@ export function Header() {
     { name: "home", href: "#hero" },
   ]
 
-  return (
+ return (
     <header className="absolute left-0 top-0 h-screen w-8 bg-background z-50">
       {/* Navigation */}
       <div className="flex flex-col items-start justify-center h-1/2 translate-y-1/2 pl-6 space-y-24">
-        {navItems.map((item) => (
+        {navItems.map((item, index) => (
           <Link
             key={item.name}
             href={item.href}
